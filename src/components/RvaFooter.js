c77d479f7cd492d14659d42d0458102aa86369ef
luisrodriguezleal <68940903+luisrodriguezleal@users.noreply.github.com>
2020-09-09 02:42:01 -0400

    Add files via upload

---
+++ src/components/RvaFooter.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class RvaFooter extends Component {
+	render () {
+		return (
+			<div className="jumbotron text-center my-2">
+				<span>Autor:</span>
+				<h2 className="h2 my-2">Luis Rodríguez Leal</h2>
+				<h5 className="h5"><a href="mailto:#">rodriguezlealluis01@gmail.com</a></h5>
+			</div>
+		)
+	}
+}
+
+export default RvaFooter;
